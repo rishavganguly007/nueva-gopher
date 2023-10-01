d73a288139bd23251ef003cf413806c0844ac8b7
rishavganguly007 <rishavganguly007@gmail.com>
2023-10-01 23:48:39 +0530

    Go examples

---
+++ GoByExample/values.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+func values() {
+	fmt.Println("go" + "lang")
+
+	fmt.Println("1+1 = ", 1+1)
+	fmt.Println("6.0/3.0 = ", 6.0/3.0)
+
+	fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
