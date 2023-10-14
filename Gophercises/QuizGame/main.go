3109d4857193b8cb36d4e89b8b63de21ad1fbb0b
rishavganguly007 <rishavganguly007@gmail.com>
2023-10-14 21:09:54 +0530

    quiz game

--- Gophercises/QuizGame/main.go
+++ Gophercises/QuizGame/main.go
@@ -1,3 +1,5 @@
+// resource: https://www.youtube.com/watch?v=FjxK1KJ7iq4
+
 package main
 
 import (
@@ -5,10 +7,13 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question-answer' ")
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
 	flag.Parse()
 
 	file, err := os.Open(*csvFilename)
@@ -20,9 +25,52 @@ func main() {
 	r := csv.NewReader(file)
 	line, err := r.ReadAll()
 	if err != nil {
-		exit("Faile to provide csv")
+		exit("Failed to provide csv")
 	}
-	fmt.Println(line)
+	problems := parselines(line)
+	fmt.Println(problems)
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("Problem %d: %s = \n", i+1, p.q)
+		answerCh := make(chan string)
+		go func(){
+			var answer string
+			fmt.Scanf("%s\n", &answer)	
+			answerCh <- answer
+		}()
+		select {
+		case <-timer.C:
+			fmt.Printf("\nTime ran out. You Scored %d out of %d.\n ",correct, len(problems))
+			return
+		case answer := <- answerCh:
+			if answer == p.a {
+				correct++
+			}
+		
+		
+	}
+	}
+	fmt.Printf("You Scored %d out of %d.\n ",correct, len(problems))
+}
+
+func parselines(lines [][]string) []problem{
+		ret := make([]problem, len(lines))
+
+		for i, line := range lines {
+			ret[i] = problem{
+				q: line[0],
+				a: strings.TrimSpace(line[1]), // handles the case, if csv file has spaces in answer
+			}
+
+		}
+		return ret
+}
+
+type problem struct {
+	q string 
+	a string
 }
 
 func exit(msg string) {
